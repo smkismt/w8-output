9123c9a214a679007e4f8dbaf47ea95b8ffd9c88
sumiki-siid5 <smk_a16@icloud.com>
2023-12-05 16:36:47 +0900

    ログインフォーム作成及びデータベースの入力まで。

--- html-css/app.js
+++ html-css/app.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+const $button = document.getElementById("js-login-button");
+
+$button.addEventListener("click", () => {
+  const $email = document.getElementById("js-login-email").value;
+  const $password = document.getElementById("js-login-password").value;
+  localStorage.setItem("email", $email);
+  localStorage.setItem("password", $password);
+});
