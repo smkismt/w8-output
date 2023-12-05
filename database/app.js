456c74e8714ac4711a59d5e2f2b1a975a6bfbfe5
sumiki-siid5 <smk_a16@icloud.com>
2023-12-05 22:17:09 +0900

    データベースの完成とcssの修正。

--- database/app.js
+++ database/app.js
@@ -1,8 +1,32 @@
+// ページを開いた際にlocalStorageにメールかパスワードがあれば代入
+window.addEventListener("load", () => {
+  const localStorageEmail = localStorage.getItem("email");
+  const localStoragePassword = localStorage.getItem("password");
+
+  // htmlのメールとパスワードの入れ物を準備
+  let $emailInput = document.getElementById("js-login-email");
+  let $passwordInput = document.getElementById("js-login-password");
+
+  // メールのデーターが代入されていればhtmlに反映、なければ空欄を維持
+  if (localStorageEmail) {
+    $emailInput.value = localStorageEmail;
+  }
+  // パスワードのデーターが代入されていればhtmlに反映、なければ空欄を維持
+  if (localStoragePassword) {
+    $passwordInput.value = localStoragePassword;
+  }
+});
+
+// htmlのボタンの入れ物を準備
 const $button = document.getElementById("js-login-button");
 
+// ボタンがクリックされたらメールとパスワードをlocalStorageに保存
 $button.addEventListener("click", () => {
   const $email = document.getElementById("js-login-email").value;
   const $password = document.getElementById("js-login-password").value;
   localStorage.setItem("email", $email);
   localStorage.setItem("password", $password);
 });
+
+// localStorageの内容を削除
+// localStorage.clear();
